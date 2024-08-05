80f05f4f13c83f96cd3ce194d81e7e2cfac7fd3c
Dingjie Cao <caodingjie@gmail.com>
2024-08-06 03:23:23 +0800

    account view

---
+++ NotchDrop/NotchAccountView.swift
@@ -0,0 +1,18 @@
+//
+//  NotchAccountView.swift
+//  NotchIsland
+//
+//  Created by 曹丁杰 on 2024/8/5.
+//
+
+import SwiftUI
+
+struct NotchAccountView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NotchAccountView()
+}
